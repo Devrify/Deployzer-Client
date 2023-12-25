6c5c3e0efbe34daadc25c9076adf9a2d1e81f8d1
houance <837507557@qq.com>
2023-12-26 00:05:59 +0800

    增加文件夹创建

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def retry(function: Callable, count:int, early_success:bool=True):
 
 if __name__ == '__main__':
     # 初始化 logger
+    os.makedirs('log', exist_ok=True)
     logging_config_file_path = os.path.join(os.path.dirname(__file__), 'logging_config.ini')
     logging.config.fileConfig(logging_config_file_path)
     logger = logging.getLogger(__name__)
