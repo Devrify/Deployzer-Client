d8442d3fdc9ddb87cbdede335ede110704a33679
houance <837507557@qq.com>
2023-12-28 22:45:23 +0800

    修改安装脚本

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ git pull;
 python3 -m venv .venv;
 . .venv/bin/activate;
 pip3 install -r requirements.txt;
-nohup python3 main.py -u http://43.139.195.214:10810 -t dt1112728325 -n prod-test &
+nohup python3 main.py -u http://43.139.195.214:10810 -t dt1112728325 -n prod-test >&/dev/null &
